32dfb30691e1bc7583dcedbd8be0c91d57f2534a
CodingCherry <cherry.alexeeva@gmail.com>
2018-12-19 18:58:12 +0700

    Custom drawing with drawRect added

---
+++ window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,53 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Alexeeva Ekaterina on 12/17/18.
+//  Copyright © 2018 Alexeeva Ekaterina. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class CurrencyTxtField: UITextField {
+    
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 0.6)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+    }
+    
+    func customizeView() {
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if placeholder == nil {
+            placeholder = " "
+        }
+        
+        let place = NSAttributedString(string: placeholder!, attributes:[.foregroundColor: UIColor.white])
+        attributedPlaceholder = place
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+
+}
